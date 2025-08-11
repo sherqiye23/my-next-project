133e06c21811e341f2861c425a6cda0dd64cfc47
Şərqiyə <145263854+sherqiye23@users.noreply.github.com>
2025-08-11 18:43:22 +0400

    admin setting page writed

---
+++ src/app/(main)/admin/settings/page.tsx
@@ -0,0 +1,9 @@
+// app/admin/dashboard/page.tsx
+
+export default function SettingsPage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">Admin setting</h1>
+        </div>
+    );
+}
